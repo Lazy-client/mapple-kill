619a9ec6844c325719cc800683782e095b916d06
Lazy <1577660832@qq.com>
2022-03-13 20:35:21 +0800

    docs(seckill): 完善接口文档

--- mapple-seckill/src/main/java/com/mapple/seckill/controller/SeckillController.java
+++ mapple-seckill/src/main/java/com/mapple/seckill/controller/SeckillController.java
@@ -3,11 +3,16 @@ package com.mapple.seckill.controller;
 import com.mapple.common.utils.CommonResult;
 import com.mapple.seckill.cons.RedisConstants;
 import com.mapple.seckill.service.SecKillService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * @author zsc
@@ -15,11 +20,18 @@ import javax.annotation.Resource;
  * @date 2022/2/17 17:58
  */
 
+@Api(tags = {"秒杀"})
 @RestController
 public class SeckillController {
     @Resource
     private SecKillService secKillService;
 
+    @ApiOperation(value = "点击秒杀",notes = "随机码一定要传")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "key", value = "秒杀产品的随机码", required = true, dataTypeClass = String.class),
+            //@ApiImplicitParam(name = "lostID", value = "ID", required = true, dataTypeClass = Long.class),
+            //@ApiImplicitParam(name = "img", value = "照片", required = true, dataTypeClass = MultipartFile.class),
+    })
     @GetMapping("kill/{key}")
     public CommonResult kill(@PathVariable String key) {
         String s = null;
@@ -36,10 +48,12 @@ public class SeckillController {
     @Resource
     private RedisConstants redisConstants;
 
+    @ApiOperation(value = "获取redis的key",notes = "这只是个测试接口，不是业务")
     @GetMapping("/redisKey")
     public CommonResult redisKey() {
-        return CommonResult
+        return Objects.requireNonNull(CommonResult
                 .ok()
-                .put("redisKey", redisConstants.getPort());
+                .put("redisKey", redisConstants.getPort()))
+                .put("redisHost",redisConstants.getHost());
     }
 }
