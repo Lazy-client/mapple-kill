d1e99281975357fbf00f2aafd9b92ebdaf77cd15
Lazy <1577660832@qq.com>
2022-03-20 21:24:36 +0800

    refactor(parent): 为consume增加自动化部署功能

--- mapple-admin/src/main/java/io/renren/common/exception/RRExceptionHandler.java
+++ mapple-admin/src/main/java/io/renren/common/exception/RRExceptionHandler.java
@@ -1,7 +1,7 @@
 package io.renren.common.exception;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.renren.common.utils.R;
 import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.dao.DuplicateKeyException;
