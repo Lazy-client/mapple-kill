2fd60510fc6317f8203ae4de30172c1815fed478
Lazy <1577660832@qq.com>
2022-03-27 15:26:23 +0800

    feat(gateway): 增加限流过滤器，统一网关层的全局限流

---
+++ mapple-gateway/src/main/java/com/mapple/gateway/config/GatewayFlowLimitConfig.java
@@ -0,0 +1,110 @@
+package com.mapple.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author zsc
+ * @version 1.0
+ * @date 2022/3/27 14:08
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.cloud.gateway")
+public class GatewayFlowLimitConfig {
+
+    private List<RouteDefinition> routes = new ArrayList<>();
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayFlowLimitConfig(ObjectProvider<List<ViewResolver>> viewResolversProvider, ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    // 初始化一个限流的过滤器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+
+    public void setRoutes(List<RouteDefinition> routes) {
+        this.routes = routes;
+    }
+
+
+    // 配置初始化的限流参数
+    @PostConstruct
+    public void initGatewayRules() {
+
+        logger.info(routes.toString());
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(new GatewayFlowRule("server-source") //资源名称,对应路由id
+                .setCount(1) // 限流阈值
+                .setIntervalSec(1) // 统计时间窗口，单位是秒，默认是 1 秒
+        );
+
+
+        Set<GatewayFlowRule> flowRules = routes
+                .stream()
+                .map(route -> {
+                            if (ServiceName.MAPPLE_SECKILL.getService().equals(route.getId()))
+                                return new GatewayFlowRule(route.getId())
+                                        .setCount(800)
+                                        .setBurst(250)
+                                        .setIntervalSec(1);
+
+                            return new GatewayFlowRule(route.getId())
+                                    .setCount(150)
+                                    .setBurst(100)
+                                    .setIntervalSec(1);
+                        }
+                ).collect(Collectors.toSet());
+        GatewayRuleManager.loadRules(flowRules);
+    }
+
+    // 配置限流的异常处理器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    // 自定义限流异常页面
+    @PostConstruct
+    public void initBlockHandlers() {
+        BlockRequestHandler blockRequestHandler = (serverWebExchange, throwable) -> {
+            Map map = new HashMap<>();
+            map.put("code", 666);
+            map.put("message", "流量过大,接口被限流了");
+            return ServerResponse.status(HttpStatus.OK).
+                    contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(map));
+        };
+        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
+    }
+}
---
+++ mapple-gateway/src/main/java/com/mapple/gateway/config/ServiceName.java
@@ -0,0 +1,26 @@
+package com.mapple.gateway.config;
+
+/**
+ * @author zsc
+ * @version 1.0
+ * @date 2022/3/27 14:52
+ */
+@SuppressWarnings("unused")
+public enum ServiceName {
+    MAPPLE_SECKILL("mapple-seckill"),
+    MAPPLE_ADMIN("renren-fast"),
+    MAPPLE_COUPON("mapple-coupon"),
+    MAPPLE_CONSUME("mapple-consumer"),
+    MAPPLE_GATEWAY("mapple-gateway");
+
+
+    private final String service;
+
+    ServiceName(String service) {
+        this.service = service;
+    }
+
+    public String getService() {
+        return this.service;
+    }
+}
