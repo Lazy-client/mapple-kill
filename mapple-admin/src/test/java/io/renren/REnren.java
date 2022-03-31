20af12b13d7492f16ffa9c0ea301ea31d4241d73
Lazy <1577660832@qq.com>
2022-03-31 12:15:58 +0800

    fix(admin): 定时任务未clear随机码

--- mapple-admin/src/test/java/io/renren/REnren.java
+++ mapple-admin/src/test/java/io/renren/REnren.java
@@ -3,6 +3,8 @@ package io.renren;
 import io.renren.common.utils.R;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @author zsc
  * @version 1.0
@@ -12,5 +14,10 @@ public class REnren {
     @Test
     public void test(){
         System.out.println(R.ok().put("1", 2));
+
+        Object[] skuKeys = new String[2];
+        skuKeys[0]="ssss";
+        skuKeys[1]="2222";
+        System.out.println(Arrays.toString(skuKeys));
     }
 }
