a719ee2fff201a3d99c0101cb271eb7fd8e07745
Lazy <1577660832@qq.com>
2022-04-02 16:29:46 +0800

    refactor(consume): status cloud bu null

--- mapple-admin/src/main/java/io/renren/modules/app/config/SpringAsyncConfig.java
+++ mapple-admin/src/main/java/io/renren/modules/app/config/SpringAsyncConfig.java
@@ -1,21 +1,16 @@
 package io.renren.modules.app.config;
 
-import io.renren.modules.app.entity.drools.UserRuleAction;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -42,7 +37,7 @@ public class SpringAsyncConfig implements AsyncConfigurer {
     /**构建线程工厂*/
     private ThreadFactory threadFactory=new ThreadFactory() {
         //CAS算法
-        private AtomicInteger at=new AtomicInteger(maximumPoolSize);
+        private final AtomicInteger at=new AtomicInteger(maximumPoolSize);
         @Override
         public Thread newThread(Runnable r) {
             return new Thread(r,
@@ -50,7 +45,7 @@ public class SpringAsyncConfig implements AsyncConfigurer {
         }
     };
     @Override
-    @Bean("myExecutor")
+    @Bean("customizedApplicationTaskExecutor")
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(corePoolSize);
@@ -58,9 +53,7 @@ public class SpringAsyncConfig implements AsyncConfigurer {
         executor.setKeepAliveSeconds(keepAliveTime);
         executor.setQueueCapacity(queueCapacity);
         executor.setRejectedExecutionHandler((Runnable r,
-                                              ThreadPoolExecutor exe) -> {
-            log.warn("当前任务线程池队列已满.");
-        });
+                                              ThreadPoolExecutor exe) -> log.warn("当前任务线程池队列已满."));
         executor.initialize();
         return executor;
     }
@@ -68,13 +61,7 @@ public class SpringAsyncConfig implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler
     getAsyncUncaughtExceptionHandler() {
-        return new AsyncUncaughtExceptionHandler() {
-            @Override
-            public void handleUncaughtException(Throwable ex ,
-                                                Method method , Object... params) {
-                log.error("线程池执行任务发生未知异常.", ex);
-            }
-        };
+        return (ex, method, params) -> log.error("线程池执行任务发生未知异常.", ex);
     }}
 
 
