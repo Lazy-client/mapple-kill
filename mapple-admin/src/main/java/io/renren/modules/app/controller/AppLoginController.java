84f8baf71a2c3d05e196f6d302c4edd718b1f022
Lazy <1577660832@qq.com>
2022-04-04 18:48:13 +0800

    refactor(admin): 使用方法IpUtils获取Ip

--- mapple-admin/src/main/java/io/renren/modules/app/controller/AppLoginController.java
+++ mapple-admin/src/main/java/io/renren/modules/app/controller/AppLoginController.java
@@ -9,6 +9,7 @@
 package io.renren.modules.app.controller;
 
 
+import io.renren.common.utils.IPUtils;
 import io.renren.common.utils.LoggerUtil;
 import io.renren.common.utils.R;
 import io.renren.modules.app.entity.UserEntity;
@@ -28,8 +29,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -68,34 +67,7 @@ public class AppLoginController {
     public R login(@RequestBody LoginForm form, HttpServletRequest request) {
         //表单校验
 //        ValidatorUtils.validateEntity(form);
-
-        //获取request中的ip
-        String ipAddress = request.getHeader("x-forwarded-for");
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("Proxy-Client-IP");
-        }
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getRemoteAddr();
-            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
-                //根据网卡取本机配置的IP
-                InetAddress inet = null;
-                try {
-                    inet = InetAddress.getLocalHost();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }
-                ipAddress = inet.getHostAddress();
-            }
-        }
-        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-        if (ipAddress != null && ipAddress.length() > 15) { //"***.***.***.***".length() = 15
-            if (ipAddress.indexOf(",") > 0) {
-                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-            }
-        }
+        String ipAddress = IPUtils.getIpAddr(request);
         LoggerUtil.getLogger().info("user IP {}", ipAddress);
         stringRedisTemplate.opsForValue().set("用户登录ip", ipAddress);
 
